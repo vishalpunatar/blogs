565e8cd73e6198fc4e53e6f60a0a36b163074a64
Vishal-Punatar <vishalsp1000z@gmail.com>
2023-12-19 11:39:34 +0530

    Update Controller

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use App\Models\Blog;
 use App\Models\User;
