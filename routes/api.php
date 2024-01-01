60248dae51853899f575def5a98187a20bb9b629
vishalpunatar <vishal@serveravatar.com>
2024-01-01 11:26:14 +0530

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Use App\Http\Controllers\SuperadminController;
 // Public Routes
 Route::post('/signup', [AuthController::class, 'store']);
 Route::post('/login',[AuthController::class,'login']);
+<<<<<<< HEAD
 Route::get('/blogs',[BlogController::class,'blogs']);
 Route::get('/blogs/{blog}',[BlogController::class,'show']);
 Route::post('/forget-password',[AuthController::class,'forgetPassword']);
@@ -54,4 +55,54 @@ Route::prefix('blogs/{blog}/')->middleware(['auth:api','UserManage'])->group(fun
 });       
 
 
+=======
+Route::get('/blog',[BlogController::class,'blog']);
+Route::get('/blogdetail/{blog}',[BlogController::class,'blogData']);
+Route::post('/forgetpassword',[AuthController::class,'forgetPassword']);
+Route::post('/reset-password/{token}',[AuthController::class,'resetPassword']);
+
+//super-admin Routes
+Route::middleware(['auth:api','IsSuperadmin','UserManage'])->prefix('/super-admin')->group(function () {    
+    Route::get('/show',[AuthController::class,'profileshow']);
+    Route::get('/userlist/{name?}',[SuperadminController::class,'userList']);
+    Route::get('/bloglist/{title?}',[SuperadminController::class,'blogList']);
+    Route::get('/publisherlist/{name?}',[SuperadminController::class,'publisherList']);
+    Route::get('/blog/request',[SuperadminController::class,'blogRequestList']);
+    Route::post('/blog/approval/{blog}',[SuperadminController::class,'blogApproval']);
+    Route::get('/publisher/request',[SuperadminController::class,'publisherRequestList']);
+    Route::post('/publisher/approval/id/{id}/token/{token}',[SuperadminController::class,'publisherApproval']);
+    Route::post('/edituser/{user}',[SuperadminController::class,'editUser']);
+    Route::post('/editblog/{blog}',[SuperadminController::class,'editBlog']);
+    Route::delete('/userdelete/{user}',[SuperadminController::class,'userDelete']);
+    Route::delete('/blogdelete/{blog}',[SuperadminController::class,'blogDelete']);
+    Route::post('/addlike/{blog}',[BlogController::class,'addLike']);
+    Route::post('/addcomment/{blog}',[BlogController::class,'addComment']);
+    Route::post('/addreply/{comment}',[BlogController::class,'addReply']);
+    Route::get('/showcomment/{blog}',[BlogController::class,'showComment']);
+    Route::get('/showlike/{blog}',[BlogController::class,'showLike']);
+    Route::get('/showreply/{blog}',[BlogController::class,'showReply']);
+    Route::post('/change/password',[AuthController::class,'changePassword']);
+    Route::post('/manage/user/{user}',[SuperadminController::class,'manageUser']);
+    Route::get('/logout',[AuthController::class,'logout']);
+    //Route::get('/fetchblog/{blog}',[SuperadminController::class,'fetchBlogForEdit']);
+});
+
+//publisher Routes
+Route::middleware(['auth:api','IsPublisher','UserManage'])->prefix('/publisher')->group(function () {
+    Route::get('/show',[AuthController::class,'profileshow']);
+    Route::post('/createblog',[BlogController::class,'createBlog']);
+    Route::post('/editblog/{blog}',[BlogController::class,'editBlog']);
+    Route::get('/myblog/{title?}',[PublisherController::class,'myBlog']);
+    Route::delete('/blogdelete/{blog}',[BlogController::class,'blogDelete']);
+    Route::post('/addlike/{blog}',[BlogController::class,'addLike']);
+    Route::post('/addcomment/{blog}',[BlogController::class,'addComment']);
+    Route::post('/addreply/{comment}',[BlogController::class,'addReply']);
+    Route::get('/showcomment/{blog}',[BlogController::class,'showComment']);
+    Route::get('/showlike/{blog}',[BlogController::class,'showLike']);
+    Route::get('/showreply/{blog}',[BlogController::class,'showReply']);
+    Route::post('/change/password',[AuthController::class,'changePassword']);
+    Route::get('/logout',[AuthController::class,'logout']);
+    //Route::get('/fetchblog/{blog}',[SuperadminController::class,'fetchBlogForEdit']);
+});
+>>>>>>> 10b77ea (Update Routes)
 
